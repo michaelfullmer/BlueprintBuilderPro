1156b8864f780c0de0a3b5e108cfe132a6c572c7
Michael Fullmer <55968597+michaelfullmer@users.noreply.github.com>
2026-01-03 21:47:13 -0500

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,3 +1,5 @@
+import { Settings } from 'lucide-react';
+import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
